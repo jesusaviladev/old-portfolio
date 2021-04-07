98ca14962ed44f81f8c11ca13763daad37ca349f
Jesus-PC <avijesus13@gmail.com>
2021-04-07 00:14:18 -0400

    nuevas mejoras

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,10 +1,30 @@
 console.log('Mi portafolio')
+
+const portfolioData = [
+	{
+		id: 1,
+		link: 'https://genteconestilomexico.com/',
+		image: 'assets/images/1.jpg',
+		thumbnail: 'assets/images/1.jpg',
+		title: 'Gente con estilo Mexico',
+		description: 'Pagina web de comercio electrónico, realizada es HTML, CSS y JavaScript'
+	},
+	{
+		id: 2,
+		link: 'https://jesusaviladev.github.io/',
+		image: 'assets/images/2.jpg',
+		thumbnail: 'assets/images/2.jpg',
+		title: 'Proyecto 2',
+		description: 'Pagina web 2'
+	},
+]
+
+
 //Menu pegajoso
 const menu = document.querySelector('.header')
 const menuHeight = menu.offsetTop;
 window.addEventListener('scroll', ()=>{
 	(pageYOffset > menuHeight) ? menu.classList.add('header__fixed') : menu.classList.remove('header__fixed')
-
 })
 
 // Menu movil
@@ -18,9 +38,11 @@ menuToggle.addEventListener('click', (e)=>{
 const gallery = document.querySelector('.gallery')
 gallery.addEventListener('click', (e)=>{
 	if(e.target.classList.contains('gallery__image')){
+		const id = e.target.dataset.id
 		const modal = document.querySelector('.modal')
 		modal.classList.add('modal--show')
 		document.body.style.overflowY = 'hidden'
+		getModalElement(id)
 
 		modal.addEventListener('click', (e)=>{
 			if(e.target.classList.contains('modal')){
@@ -30,3 +52,33 @@ gallery.addEventListener('click', (e)=>{
 		})
 	}
 })
+//funcion 
+
+//formulario
+const form = document.querySelector('.form')
+form.addEventListener('submit', (e)=>{
+	console.log(e)
+	e.preventDefault();
+})
+
+function getModalElement(id){
+	const modalElement = portfolioData.filter((elements)=> elements.id == id)
+	const fragment = document.createDocumentFragment()
+	const template = document.getElementById('template').content
+	const container = document.querySelector('.modal__container')
+
+	while(container.firstElementChild){
+		container.removeChild(container.lastElementChild)
+	}
+
+	template.querySelector('.modal__title').textContent = modalElement[0].title
+	template.querySelector('.modal__image').src = modalElement[0].image
+	template.querySelector('.modal__description').textContent = modalElement[0].description
+	template.querySelector('.modal__link').href = modalElement[0].link
+
+	let newTemplate = template.cloneNode(true)
+	fragment.appendChild(newTemplate)
+
+	container.appendChild(fragment)
+
+}
